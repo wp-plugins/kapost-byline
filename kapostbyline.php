107156fab8efed73fd8f3180f19d9f864ba0c1dc
iamnader <iamnader@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-10 13:06:26 +0000

    tweaks
    git-svn-id: https://plugins.svn.wordpress.org/kapost-byline/trunk@487485 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kapostbyline.php
+++ kapostbyline.php
@@ -3,11 +3,11 @@
 	Plugin Name: Kapost Social Publishing Byline
 	Plugin URI: http://www.kapost.com/
 	Description: Kapost Social Publishing Byline
-	Version: 1.0.5
+	Version: 1.0.6
 	Author: Kapost
 	Author URI: http://www.kapost.com
 */
-define('KAPOST_BYLINE_VERSION', '1.0.5');
+define('KAPOST_BYLINE_VERSION', '1.0.6');
 define('KAPOST_BYLINE_WP3', (get_bloginfo('version') >= 3.0));
 define('KAPOST_BYLINE_FILENAME', __FILE__);
 define('KAPOST_BYLINE_BASEPATH', dirname(__FILE__));
